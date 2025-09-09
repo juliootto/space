745a0c4f2fbbc453db9c3850616cfcc57c840889
julootto <juliootto@gmail.com>
2025-09-08 23:20:30 -0300

    ajustado funcionalidade buscar

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from galeria.models import Fotografia
+
 
 # Create your views here.
-def index(request):
-        return render(request, 'galeria/index.html')
+def index(request,filtro_categoria=None):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
+    if filtro_categoria:
+        fotografias = fotografias.filter(categoria=filtro_categoria)
+    return render(request, 'galeria/index.html',{'cards':fotografias})
 
-def imagem(request):
-        return render(request, 'galeria/imagem.html')
+def imagem(request,foto_id):
+    fotografia = get_object_or_404(Fotografia,pk=foto_id) 
+    return render(request, 'galeria/imagem.html',{'fotografia':fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicado=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar: 
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, 'galeria/index.html',{'cards':fotografias})
--- setup/urls.py
+++ setup/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
@@ -23,4 +26,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('galeria.urls') ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
