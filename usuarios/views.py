3221ad346d5e1604ac93c436827bb3cef05a2220
juliootto <52258494+juliootto@users.noreply.github.com>
2025-09-12 10:03:24 -0300

    ajustado o formulário de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,15 @@
+"""
+Módulo de views para gerenciar autenticação de usuários.
+
+Este módulo contém as funções de view para:
+- `login`: Exibe o formulário de login e processa a autenticação do usuário.
+- `logout`: Realiza o logout do usuário.
+- `cadastrar`: Exibe o formulário de cadastro e processa o registro de novos usuários.
+"""
 from django.shortcuts import redirect, render
 from usuarios.forms import LoginForm, CadastroForms
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
@@ -14,7 +24,21 @@ def cadastrar(request):
     form = CadastroForms()
     if request.method == 'POST':
         form = CadastroForms(request.POST)
-
-        if form["senha_1"].value() != form["senha_2"].value():
-            return redirect ('cadastro')
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect ('cadastrar')
+        
+            nome = form.cleaned_data['nome_cadastro']
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha_1']
+            
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastrar')
+            
+            usuario = User.objects.create_user(username=nome,email=email,password=senha)
+            usuario.save()
+            
+            return redirect('login')
+        else:
+            form = CadastroForms()
     return render(request,'usuarios/cadastrar.html',{'form':form})
